7832e16d8f18786a97938445d33f89d6f53d3563
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-08-18 19:30:20 +0200

    Add VSI site layout

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Visionary Students Initiative | Zambia",
+  description: "Promoting policies and initiatives that place students at the centre of national development.",
+};
+
+export default function RootLayout({ children }) {
+  return <html lang="en"><body>{children}</body></html>;
+}
