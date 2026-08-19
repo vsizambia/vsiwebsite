b43e1a93e1fc8192f6122c8c6b7be1605f3c8b20
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-08-19 21:32:40 +0200

    Add robots metadata route for SEO

---
+++ app/robots.js
@@ -0,0 +1,7 @@
+export default function robots() {
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://www.vsizambia.org/sitemap.xml",
+    host: "https://www.vsizambia.org",
+  };
+}
