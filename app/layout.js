9e55ffa3f1012669597730d2515e6c04de09eebf
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-08-19 21:46:23 +0200

    Strengthen site-wide SEO and organization metadata

--- app/layout.js
+++ app/layout.js
@@ -5,9 +5,12 @@ import "./volunteer-enhancements.css";
 import "./polish.css";
 import "./mobile.css";
 import "./accessibility.css";
+import "./qa.css";
+
+const siteUrl = "https://vsizambia.org";
 
 export const metadata = {
-  metadataBase: new URL("https://www.vsizambia.org"),
+  metadataBase: new URL(siteUrl),
   title: {
     default: "Visionary Students Initiative | Zambia",
     template: "%s | Visionary Students Initiative",
@@ -21,7 +24,7 @@ export const metadata = {
     siteName: "Visionary Students Initiative",
     title: "Visionary Students Initiative | Zambia",
     description: "Promoting policies and initiatives that place students at the centre of national development.",
-    url: "https://www.vsizambia.org/",
+    url: siteUrl,
     images: [
       {
         url: "/images/vsi-parliament.jpg",
@@ -43,12 +46,40 @@ export const metadata = {
   },
 };
 
+const organizationSchema = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  name: "Visionary Students Initiative",
+  alternateName: "VSI",
+  url: siteUrl,
+  logo: `${siteUrl}/images/vsi logo blue text.png`,
+  description: "A Zambian youth-led organisation working to promote policies and initiatives that place students and young people at the centre of national development.",
+  email: "info@vsizambia.org",
+  telephone: "+260974140217",
+  sameAs: [
+    "https://web.facebook.com/vsizambia",
+    "https://www.instagram.com/vsizambia",
+    "https://www.youtube.com/@vsizambia",
+    "https://www.linkedin.com/in/visionary-students-initiative-vsi-0a4472382/",
+  ],
+  address: {
+    "@type": "PostalAddress",
+    streetAddress: "Plot No. 9173, Ben Bella Road, Peace Embassy Building, 1st Floor",
+    addressLocality: "Lusaka",
+    addressCountry: "ZM",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
         <a className="skip-link" href="#main-content">Skip to main content</a>
         <div id="main-content">{children}</div>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+        />
       </body>
     </html>
   );
