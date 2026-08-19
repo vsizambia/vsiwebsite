7780f7818fe0d88bc3566be59ed17155fd7f6d82
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-08-19 21:32:15 +0200

    Add Our Story page SEO metadata

---
+++ app/story/layout.js
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Our Story",
+  description: "Follow the story of Visionary Students Initiative from a student idea to a growing movement for national development.",
+  alternates: { canonical: "/story" },
+};
+
+export default function StoryLayout({ children }) {
+  return children;
+}
