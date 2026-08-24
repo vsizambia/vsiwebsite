27a07b928f9f73a84c57e105f04ebf6aef73e5d5
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-08-24 10:08:02 +0200

    Fix remote News images in Next Image

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+        pathname: "/news/**",
+      },
+    ],
   },
 };
 
