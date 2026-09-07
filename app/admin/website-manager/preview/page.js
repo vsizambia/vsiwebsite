895a969431f08e331b97910157489811612155eb
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-09-07 21:59:38 +0200

    fix: correct website builder preview component imports

--- app/admin/website-manager/preview/page.js
+++ app/admin/website-manager/preview/page.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 import { cookies } from "next/headers";
 import { pool } from "../../../../lib/db";
 import { isAdminAuthenticated } from "../../../../lib/admin-auth";
-import { SiteHeader, SiteFooter } from "../../../../components/SiteChrome";
-import WebsiteBuilderRenderer from "../../../../components/WebsiteBuilderRenderer";
+import { SiteHeader, SiteFooter } from "../../../components/SiteChrome";
+import WebsiteBuilderRenderer from "../../../components/WebsiteBuilderRenderer";
 
 export const metadata = {
   title: "Website Builder Preview | VSI",
