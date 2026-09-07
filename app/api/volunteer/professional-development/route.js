b57ec411717a6a772b89bcaa8035cc492687ca74
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-09-07 12:13:12 +0200

    Fix professional development API database import

--- app/api/volunteer/professional-development/route.js
+++ app/api/volunteer/professional-development/route.js
@@ -1,5 +1,5 @@
 import {NextResponse} from "next/server";
-import {ensureVolunteerTable,pool} from "../../../../../lib/db";
+import {ensureVolunteerTable,pool} from "../../../../lib/db";
 
 async function ensureTable(){
  await ensureVolunteerTable();
