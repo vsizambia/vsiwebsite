1f8c14132ea73b150b06d4fb6b2fe2282ab84092
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-09-13 10:26:38 +0200

    feat: move volunteer Turnstile before submit button

--- app/volunteer/apply/layout.js
+++ app/volunteer/apply/layout.js
@@ -1,11 +1,9 @@
 import { SiteFooter, SiteHeader } from "../../components/SiteChrome";
-import VolunteerApplicationTurnstile from "./VolunteerApplicationTurnstile";
 
 export default function VolunteerApplicationLayout({ children }) {
   return (
     <>
       <SiteHeader ctaLabel="Volunteer" ctaHref="/volunteer/apply" />
-      <VolunteerApplicationTurnstile />
       {children}
       <SiteFooter />
     </>
