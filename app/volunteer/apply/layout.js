246d1d4d20f59ed19347207f381ff7c2a59c8336
Visionary Students Initiative (VSI) <vsizambia@gmail.com>
2026-09-13 06:31:13 +0200

    feat: render volunteer application security check

--- app/volunteer/apply/layout.js
+++ app/volunteer/apply/layout.js
@@ -1,10 +1,12 @@
 import { SiteFooter, SiteHeader } from "../../components/SiteChrome";
+import VolunteerApplicationTurnstile from "./VolunteerApplicationTurnstile";
 
 export default function VolunteerApplicationLayout({ children }) {
   return (
     <>
       <SiteHeader ctaLabel="Volunteer" ctaHref="/volunteer/apply" />
       {children}
+      <VolunteerApplicationTurnstile />
       <SiteFooter />
     </>
   );
